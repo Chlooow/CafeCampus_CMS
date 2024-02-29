6bfc274ac939ae9b4949cd896c1e416975b3aec8
Eclow <93141020+Chlooow@users.noreply.github.com>
2024-03-01 00:06:52 +0100

    Update CafeMachine.java

--- cms/src/CafeMachine.java
+++ cms/src/CafeMachine.java
@@ -2,47 +2,47 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class CafeMachine {
-    private static HashMap<String, Boolean> machines; //Ensemble des machines à café avec leur nom et leur état 
+    private HashMap<String, Boolean> machines; //Ensemble des machines à café avec leur nom et leur état
 
-    public CafeMachine(){
+    //pourquoi un static ?????
+    public CafeMachine() {
         machines = new HashMap<>();
-        machines.put("Moissan",true);
-        machines.put("Lieu de Vie",true);
-        machines.put("PUIO",true);
+        machines.put("moissan", true);
+        machines.put("lieudevie", true);
+        machines.put("puio", true);
     }
 
     //Si une machine est déja repertoriée
-    public boolean exists(String nom){
-        if(machines.containsKey(nom)){
-            return true;
-        }else{
-            return false;
-        }
+    //Note Amine: fonction inutile, sinon tu pourrais la contracter comme ceci :
+    public boolean exists(String nom) {
+        return machines.containsKey(nom);
     }
 
     //Vérifier le statut d'une machine
-    public boolean status(String nom){
+    public boolean status(String nom) {
         return machines.get(nom);
     }
 
-    public void update(String nom, boolean statut){
-        if(this.exists(nom)){
-            machines.put(nom,statut);
+    public void update(String nom, boolean statut) {
+        if (this.exists(nom)) {// pourquoi repasser par exist ? HashMap a une méthode contain...
+            machines.put(nom, statut);
             System.out.println("Le statut de la machine " + nom + " a ete mis a jour avec succes !");
-        }else{
+        } else {
             System.out.println("Mise a jour du statut de la machine " + nom + " a echoue.");
+            // possiblement throw une error ?
         }
     }
 
-    public void affichageMachine(){
-        for (String nom: machines.keySet()){
+    public void affichageMachine() {
+        for (String nom : machines.keySet()) {
             boolean statut = machines.get(nom);
             System.out.println(nom + " - Statut : " + (statut ? "En marche" : "Hors Service"));
+            //chatgpt ?
         }
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Bienvenue sur CafeCampus !\n");
         System.out.println("Prenez votre pause cafe sans stress avec Cafe Campus !\n");
         CafeMachine ensembleMachines = new CafeMachine();
@@ -53,84 +53,26 @@ public class CafeMachine {
         ensembleMachines.affichageMachine();
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Souhaitez-vous modifier le statut d'une machine ?");
-        boolean reponse = Boolean.parseBoolean(sc.nextLine());
+        boolean reponse = true;
+        while(true) {
+            System.out.println("Souhaitez-vous modifier le statut d'une machine ? Entrez oui ou non");
+            String oui_non = sc.nextLine().toLowerCase();
+            if(oui_non.equals("non")){
+                System.out.println("Ok bye");
+                return;
+            }
 
-        if (reponse == true) {
             Scanner sc1 = new Scanner(System.in);
             System.out.println("Quel machine voulez-vous modifier ?");
-            int machine = Integer.parseInt(sc1.nextLine());
-            switch (machine) {
-                case 1:
-                    if (ensembleMachines.status("Moissan") == true) {
-                        ensembleMachines.update("Moissan", false);
-                    } else {
-                        ensembleMachines.update("Moissan", true);
-                    }
-                    break;
-                case 2:
-                    if (ensembleMachines.status("Lieu de Vie") == true) {
-                        ensembleMachines.update("Lieu de Vie", false);
-                    } else {
-                        ensembleMachines.update("Lieu de Vie", true);
-                    }
-                    break;
-                case 3:
-                    if (ensembleMachines.status("PUIO") == true) {
-                        ensembleMachines.update("PUIO", false);
-                    } else {
-                        ensembleMachines.update("PUIO", true);
-                    }
-                    break;
-
+            String machine =sc1.nextLine().toLowerCase();
+            if(!ensembleMachines.exists(machine)){
+                System.out.println("Cette machine n'existe pas!");
             }
-
-            ensembleMachines.affichageMachine();
-
-            Scanner sc2 = new Scanner(System.in);
-            System.out.println("Voulez-vous modifier une autre machine ?");
-            boolean reponse2 = Boolean.parseBoolean(sc2.nextLine());
-
-            if (reponse2 == true) {
-                Scanner sc3 = new Scanner(System.in);
-                System.out.println("Quel machine voulez-vous modifier ?");
-                int machine2 = Integer.parseInt(sc3.nextLine());
-                switch (machine2) {
-                    case 1:
-                        if (ensembleMachines.status("Moissan") == true) {
-                            ensembleMachines.update("Moissan", false);
-                        } else {
-                            ensembleMachines.update("Moissan", true);
-                        }
-                        break;
-                    case 2:
-                        if (ensembleMachines.status("Lieu de Vie") == true) {
-                            ensembleMachines.update("Lieu de Vie", false);
-                        } else {
-                            ensembleMachines.update("Lieu de Vie", true);
-                        }
-                        break;
-                    case 3:
-                        if (ensembleMachines.status("PUIO") == true) {
-                            ensembleMachines.update("PUIO", false);
-                        } else {
-                            ensembleMachines.update("PUIO", true);
-                        }
-                        break;
-
-                }
-
+            if(ensembleMachines.exists(machine)) {
+                ensembleMachines.update(machine, !ensembleMachines.status(machine));
+                System.out.println(" \n");
                 ensembleMachines.affichageMachine();
-
-            }else{
-                System.out.println("Merci, et a bientot sur CafeCampus");
-            }
-        }else{
-                System.out.println("Merci, et a bientot sur CafeCampus");
             }
         }
     }
-
-
-
-
+}
