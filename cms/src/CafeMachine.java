562caf4946de718b9fc14c455472b1693e7da622
Eclow <93141020+Chlooow@users.noreply.github.com>
2024-03-13 15:41:51 +0100

    Le bon code java pour le rendu

--- cms/src/CafeMachine.java
+++ cms/src/CafeMachine.java
@@ -1,78 +1,56 @@
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.Map;
 
 public class CafeMachine {
-    private HashMap<String, Boolean> machines; //Ensemble des machines à café avec leur nom et leur état
+    private static HashMap<String, Boolean> machines; //Ensemble des machines à café avec leur nom et leur état 
 
-    //pourquoi un static ?????
-    public CafeMachine() {
+    public CafeMachine(){
         machines = new HashMap<>();
-        machines.put("moissan", true);
-        machines.put("lieudevie", true);
-        machines.put("puio", true);
+        machines.put("Moissan",true);
+        machines.put("Lieu de Vie",true);
+        machines.put("PUIO",true);
     }
 
     //Si une machine est déja repertoriée
-    //Note Amine: fonction inutile, sinon tu pourrais la contracter comme ceci :
-    public boolean exists(String nom) {
-        return machines.containsKey(nom);
+    public boolean exists(String nom){
+        if(machines.containsKey(nom)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     //Vérifier le statut d'une machine
-    public boolean status(String nom) {
+    public boolean status(String nom){
         return machines.get(nom);
     }
 
-    public void update(String nom, boolean statut) {
-        if (this.exists(nom)) {// pourquoi repasser par exist ? HashMap a une méthode contain...
-            machines.put(nom, statut);
-            System.out.println("Le statut de la machine " + nom + " a ete mis a jour avec succes !");
-        } else {
-            System.out.println("Mise a jour du statut de la machine " + nom + " a echoue.");
-            // possiblement throw une error ?
+    public void update(String nom, boolean statut){
+        if(this.exists(nom)){
+            machines.put(nom,statut);
+            System.out.println("Le statut de la machine" + nom + "a été mis à jour avec succès !");
+        }else{
+            System.out.println("Mise à jour du statut de la machine" + nom + " a échoué.");
         }
     }
 
-    public void affichageMachine() {
-        for (String nom : machines.keySet()) {
-            boolean statut = machines.get(nom);
-            System.out.println(nom + " - Statut : " + (statut ? "En marche" : "Hors Service"));
-            //chatgpt ?
-        }
-    }
+    
 
-
-    public static void main(String[] args) {
-        System.out.println("Bienvenue sur CafeCampus !\n");
-        System.out.println("Prenez votre pause cafe sans stress avec Cafe Campus !\n");
+    public static void main(String[] args){
+        System.out.println("Bienvenue sur CaféCampus !\n");
+        System.out.println("Prenez votre pause café sans stress avec Café Campus !\n");
         CafeMachine ensembleMachines = new CafeMachine();
 
         // Afficher les machines à café et leur statut
-        System.out.println("Voici les machines a cafe et leurs statuts que nous mettons a votre disposition ! :\n");
-
-        ensembleMachines.affichageMachine();
-
-        Scanner sc = new Scanner(System.in);
-        boolean reponse = true;
-        while(true) {
-            System.out.println("Souhaitez-vous modifier le statut d'une machine ? Entrez oui ou non");
-            String oui_non = sc.nextLine().toLowerCase();
-            if(oui_non.equals("non")){
-                System.out.println("Ok bye");
-                return;
-            }
-
-            Scanner sc1 = new Scanner(System.in);
-            System.out.println("Quel machine voulez-vous modifier ?");
-            String machine =sc1.nextLine().toLowerCase();
-            if(!ensembleMachines.exists(machine)){
-                System.out.println("Cette machine n'existe pas!");
-            }
-            if(ensembleMachines.exists(machine)) {
-                ensembleMachines.update(machine, !ensembleMachines.status(machine));
-                System.out.println(" \n");
-                ensembleMachines.affichageMachine();
-            }
+        System.out.println("Voici les machines à café et leurs statuts que nous mettons à votre disposition ! :\n");
+        
+        for (String nom: ensembleMachines.machines.keySet()){
+            boolean statut = ensembleMachines.machines.get(nom);
+            System.out.println(nom + " - Statut : " + (statut ? "En marche" : "Hors Service"));
         }
     }
+
+
 }
+
+
